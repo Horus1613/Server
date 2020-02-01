ccf1e4f44dcaced7b472a612d5c381f8bc2387fa
Horus1613 <47709487+Horus1613@users.noreply.github.com>
2020-02-01 22:39:34 +0300

    Revert "Revert "Builded Maven project""
    This reverts commit 39e547b33a350ff9435f1e0dfe940340202dfdac.

--- src/main/java/servlets/HelloServlet.java
+++ src/main/java/servlets/HelloServlet.java
@@ -1,14 +1,10 @@
 package servlets;
 
-import templater.PageGenerator;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 public class HelloServlet extends HttpServlet {
     @Override
@@ -17,4 +13,6 @@ public class HelloServlet extends HttpServlet {
             resp.setContentType("text/html;charset=utf-8");
             resp.setStatus(HttpServletResponse.SC_OK);
     }
+
+
 }
