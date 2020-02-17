aba2fb1b54b08c7df4262338ca7e97ea4904381c
Horus1613 <47709487+Horus1613@users.noreply.github.com>
2020-02-17 13:28:10 +0300

    refactoring

--- src/main/java/servlets/signs/SignInServlet.java
+++ src/main/java/servlets/signs/SignInServlet.java
@@ -4,7 +4,6 @@ import crypt.CipherHelper;
 import dao.UserDAO;
 import models.User;
 
-import javax.crypto.Cipher;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
